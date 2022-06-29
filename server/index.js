b517409b03b69c32f3d5e09e1c8a138c731e9cae
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-06-29 16:21:43 -0700

    install csv

--- server/index.js
+++ server/index.js
@@ -4,6 +4,22 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+import assert from 'assert';
+import { parse } from 'csv-parse/sync';
+
+const input = `
+"key_1","key_2"
+"value 1","value 2"
+`;
+const records = parse(input, {
+  columns: true,
+  skip_empty_lines: true
+});
+assert.deepStrictEqual(
+  records,
+  [{ key_1: 'value 1', key_2: 'value 2' }]
+);
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
