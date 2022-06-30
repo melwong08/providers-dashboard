8b5bdb35a3472afab4f82de34cae46c701d16f71
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-06-30 11:07:14 -0700

    Update server.js

--- node/src/server.js
+++ node/src/server.js
@@ -1,3 +1,11 @@
 const PORT = process.env.PORT || 3001;
 
 const app = require("./app");
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
