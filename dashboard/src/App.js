aca8eaf0f804be2b284e40cd1ad3a27215db40bc
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-07-08 13:45:44 -0700

    Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import Table from "./table/Table";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="App">
+      <header className="App-header container">
+        <h1>Providers Dashboard</h1>
+      </header>
+      <div className="container">
+        <Table />
+      </div>
+    </div>
+  )
 }
 
 export default App;
