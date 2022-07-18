06d119bce598e63b490418f89006efdcfdf9a8ad
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-07-18 10:22:47 -0700

    Update main.js

--- node/src/data/main.js
+++ node/src/data/main.js
@@ -1,21 +1,21 @@
 import { providers, jobs } from './data.js'
 
-for (let i = 0; i < providers.length; i++) {
-    const provider = providers[i];
-    console.log(provider.fullName)
-}
+// for (let i = 0; i < providers.length; i++) {
+//     const provider = providers[i];
+//     console.log(provider.fullName)
+// }
 
-for (let i = 0; i < jobs.length; i++) {
-    const job = jobs[i];
-    console.log(job.id + ' = ' +job.status)
-}
+// for (let i = 0; i < jobs.length; i++) {
+//     const job = jobs[i];
+//     console.log(job.id + ' = ' +job.status)
+// }
 
 for (let i = 0; i < jobs.length; i++) {
     const provider = providers[i];
     for (let j = 0; j < jobs.length; j++){
         const job = jobs[j];
         if (job.providerRating === true){
-            
+            console.log(provider.fullName)
         }
     }
 }
