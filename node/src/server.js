6e39a96892c900f33b4a2b3d2d3c4dc8639c89c1
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-07-18 13:45:20 -0700

    Update server.js

--- node/src/server.js
+++ node/src/server.js
@@ -1,14 +1,21 @@
 const PORT = process.env.PORT || 3001;
 
 const app = require("./app");
+const data = require("./data/data.js");
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
+
+app.get("/jobs", (req, res) => {
+    res.json({data})
+  })
   
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
+
+
 
 //create jobs routes & display all jobs and provider info on server page
 //query the jobs for scheduled jobs
