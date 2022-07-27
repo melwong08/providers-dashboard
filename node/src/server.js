50c4b351bb849e8db9c73269f4cf0a16a6d1069e
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-07-27 10:46:03 -0700

    Update server.js

--- node/src/server.js
+++ node/src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-//import {providers, jobs} from './data/data';
-//const data = import("./data/data")
+//import {providers, jobs} from '../data/data';
+const data = import("../data/data.js")
 
 const app = express();
 
@@ -10,19 +10,34 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
 
-app.get("/jobs/:id", (req, res) => {
-  console.log(req.query);
-  const jobId = req.query.id;
-  res.send(jobId);
+app.get("/jobs:id", (req, res) => {
+  console.log(req.params.id);
+  const jobId = req.params.id;
+  const job = findJobId(data.jobs, jobId);
+
+  if (job === null) res.status(404).send("Not Found")
+  
+  res.send(data);
   });
+
+app.get("jobs", (req, res) => {
+  res.send(data.jobs);
+})
   
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
 
+function findJobId(jobs, id){
+  for (let i = 0; i < jobs.length; i++){
+    if(jobs[i].id === id) return jobs;
+  }
+  return null;
+}
 
 
 //create jobs routes & display all jobs and provider info on server page
 //query the jobs for scheduled jobs
 //can also query for   
 //start separating out functions and layers, carve out single path for routes
+//steno.com/jobs/?id=1
