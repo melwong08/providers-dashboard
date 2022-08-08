525794c0356700162fbf51d9e205f9b6b307fd55
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-08-08 08:52:11 -0400

    Update server.js

--- node/src/server.js
+++ node/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {providers, jobs} from './data/data.js';
+import { providers, jobs } from './data/data.js';
 // const data = import("./data.js")
 // const jobs = import("../data/jobs.js");
 // const providers = import("../data/providers.js")
@@ -9,40 +9,48 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+  res.json({ message: "Hello from server!" });
+});
 
-app.get("/jobs/:status", (req, res) => {
-  const status = req.params.id;
-  const 
+app.get("/jobs/scheduled", (req, res) => {
+  const currentStatus = req.params.status;
+  const scheduled = findStatus(jobs, currentStatus);
+
+  res.json(scheduled)
 })
 
 app.get("/jobs/:id", (req, res) => {
-  console.log(req.params.id);
+  //console.log(req.params.id);
   const jobId = req.params.id;
   const job = findJobId(jobs, jobId);
-  const scheduledJob = findScheduledJob();
+  // const scheduledJob = findScheduledJob();
 
-  if (job === null) res.status(404).send("Not Found")
-  
+  if (job === null) res.status(404).send("Not Found");
+
   res.json(job);
-  });
+});
 
 app.get("/jobs", (req, res) => {
   res.json(jobs);
 })
-  
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
 
-function findJobId(jobs, id){
-  for (let i = 0; i < jobs.length; i++){
-    if(jobs[i].id == id) return jobs[i];
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+function findJobId(jobs, id) {
+  for (let i = 0; i < jobs.length; i++) {
+    if (jobs[i].id == id) return jobs[i];
   }
   return null;
 }
 
+function findStatus(jobs, scheduled) {
+  for (let i = 0; i < jobs.length; i++) {
+    if (jobs[i].status === "SCHEDULED") return jobs[i];
+  }
+  return null;
+}
 
 //create jobs routes & display all jobs and provider info on server page
 //query the jobs for scheduled jobs
