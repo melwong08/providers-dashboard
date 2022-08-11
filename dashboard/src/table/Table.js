f179205ffbf0b7dd1595ee0a02a237776efe17a1
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-08-11 11:43:40 -0400

    Add react Table

--- dashboard/src/table/Table.js
+++ dashboard/src/table/Table.js
@@ -1,9 +1,50 @@
-import React, { useState } from "react";
+//import { usePagination, useTable, Column } from "react-table";
+import "./Table.css";
 
-function Table (){
-    <div>
-        <h2>Table</h2>
-    </div>
+function Table() {
+    return (
+        <div>
+            <h1>Jobs</h1>
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Date/Time</th>
+                    <th>Status</th>
+                    <th>Provider Id</th>
+                    <th>Average Cost per Page (cents)</th>
+                    <th>Materials Turned in At</th>
+                    <th>Provider Rating</th>
+                    <th>Location Type</th>
+                    <th>Latitude</th>
+                    <th>Longitude</th>
+                </tr>
+                <tr>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                </tr>
+                <tr>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                    <td>Table item</td>
+                    <td>Table item 2</td>
+                </tr>
+            </table>
+        </div>
+    )
 }
 
 export default Table;
