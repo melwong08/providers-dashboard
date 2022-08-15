293041bb7c88c98ba2f4c6e32975e813b4eb7989
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-08-15 16:34:21 -0400

    Update Table.js

--- dashboard/src/table/Table.js
+++ dashboard/src/table/Table.js
@@ -1,21 +1,40 @@
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
 import "./Table.css";
 
+const jobsData = [
+    {id: 1, datetime: "2021-06-03 08:19:33", status: "COMPLETE", provider_id: 1, avg_cost_per_page: 550, materials_turned_in_at: "2021-06-13 10:43:33", provider_rating: true, location_type: "REMOTE", latitude: null, longitude: null}
+]
+
 function Table(props) {
-    const [data, setData] = useState(null);
+    // const [data, setData] = useState(null);
 
-    async function fetchJobsData(id){
-        const response = await fetch("/" + id);
-        setData(await response.json)
-    }
+    // async function fetchJobsData(id){
+    //     const response = await fetch("/" + id);
+    //     setData(await response.json)
+    // }
 
-    useEffect(() => {
-        fetchJobsData(props.id);
-      }, [props.id]);
+    // useEffect(() => {
+    //     fetchJobsData(props.id);
+    //   }, [props.id]);
 
-      if (!data) {
-        return "loading...";
-      }
+    //   if (!data) {
+    //     return "loading...";
+    //   }
+
+    const rows = jobsData.map(({id, datetime, status, provider_id, avg_cost_per_page, materials_turned_in_at, provider_rating, location_type, latitude, longitude}, index) => (
+        <tr key={index}>
+            <td>{id}</td>
+            <td>{datetime}</td>
+            <td>{status}</td>
+            <td>{provider_id}</td>
+            <td>{avg_cost_per_page}</td>
+            <td>{materials_turned_in_at}</td>
+            <td>{provider_rating}</td>
+            <td>{location_type}</td>
+            <td>{latitude}</td>
+            <td>{longitude}</td>
+        </tr>
+    ))
 
     return (
         <div>
@@ -36,18 +55,7 @@ function Table(props) {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                        <td>table data 2</td>
-                    </tr>
+                    {rows}
                 </tbody>
             </table>
         </div>
