6a8cf22a5ae988043758eb421a102cad46ea251e
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-09-21 11:42:58 -0400

    remove axios

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:3001";
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = "http://localhost:3001";
 
 ReactDOM.render(
   <React.StrictMode>
